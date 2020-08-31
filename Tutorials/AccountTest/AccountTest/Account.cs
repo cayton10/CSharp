0af6f5420d6537f7f999fdfa5524a159ebb7b168
cayton <cayto@DESKTOP-9EF45MF>
2020-08-31 13:12:04 -0400

    Finished Ch. 4 example of simple set and get functions for account object.

---
+++ Tutorials/AccountTest/AccountTest/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountTest
+{
+    //Public class Account available system wide
+    public class Account
+    {
+        //Name of account holder
+        //private string name;
+        //Getter returns string
+        //public string GetName()
+        //{
+        //    return name;
+        //}
+        //Setter void function
+        //public void SetName(string accountName)
+        //{
+        //    name = accountName;
+        //}
+
+        //Name property
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //Balance property
+        private decimal balance;
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set { //balance = value; 
+                if (value >= 0)
+                {
+                    balance = value;
+                }
+            }
+        }
+
+        public void Deposit(decimal depositAmount)
+        {
+            if(depositAmount > 0)
+                Balance = Balance + depositAmount;
+        }
+
+        //Default constructor
+        public Account(string accountName) 
+        {
+            Name = accountName;
+        }
+
+    }
+}
