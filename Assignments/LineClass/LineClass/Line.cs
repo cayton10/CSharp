c308f14c1ff98a090be24837a498f61ac524e47c
Obi-Wan <cayton10@live.marshall.edu>
2020-09-02 13:11:10 -0400

    Program Complete

--- Assignments/LineClass/LineClass/Line.cs
+++ Assignments/LineClass/LineClass/Line.cs
@@ -33,6 +33,19 @@ namespace Assignment1
         /************CLASS METHODS*******************/
         /********************************************/
 
+        //Number input validation method
+        public double ValidateNum(string input)
+        {
+            double value;//Value to return
+            while (!double.TryParse(input, out value))
+            {
+                Console.WriteLine("Not a number, try again.");
+                input = Console.ReadLine();
+            }
+
+            return value;
+        }
+
         //Returns value of X for given Y value
         public double ReturnX(double value)
         {
@@ -59,18 +72,20 @@ namespace Assignment1
             //Declare slope variable
             double m = 0;//Must be initialized, or won't compile
 
-            if (b != 0)
-            {
-                m = ((a * -1) / b);
-            }
-            else if (a == 0)
+            if (a == 0)
             {
                 m = 0;//Value for horizontal slope
+            }
+            else if (b != 0)
+            {
+                m = ((a * -1) / b);
             }
             else
                 //Else slope is undefined
                 m = double.PositiveInfinity;
 
+            //Format number
+            m = Math.Round(m, 2);
             return m;
         }
 
@@ -79,12 +94,41 @@ namespace Assignment1
         {
             bool parallel;//Declare variable to return
 
-            if (val1 == val2)
-                parallel = true;
-            else
-                parallel = false;
+            //Ternary control for parallel lines
+            parallel = val1 == val2 ? true : false;  
 
             return parallel;
+        }
+
+        //Determine whether two lines are perpendicular
+        public bool Perpendicular(double val1, double val2)
+        {
+            bool perpendicular;//Declare variable to return
+
+            if (val1 * val2 == -1)
+                perpendicular = true;
+            else if (val1 == 0 && val2 == double.PositiveInfinity)
+                perpendicular = true;
+            else if (val1 == double.PositiveInfinity && val2 == 0)
+                perpendicular = true;
+            else
+                perpendicular = false;
+
+            return perpendicular;
+        }
+
+        //Determine the point of intersection if lines are note parallel
+        public void Intersect(double a1, double b1, double c1, double a2, double b2, double c2)
+        {
+            double pointX;//Declare point in 2D space to return
+            //Find x intersection point formula
+            pointX = Math.Round((((b1 * c2) - (b2 * c1)) / ((a1 * b2) - (a2 * b1))), 2);
+
+            double pointY;
+            //Find y intersection point formula
+            pointY = Math.Round((((a2 * c1) - (a1 * c2)) / ((a1 * b2) - (a2 * b1))), 2);
+
+            Console.WriteLine($"The intersection point of the two lines is: (x,y) = ({pointX}, {pointY}).");
         }
     }
 }
--- Assignments/LineClass/LineClass/Program.cs
+++ Assignments/LineClass/LineClass/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//Name: Benjamin Cayton
+//Course: CIT265 - C# Programming
+//Date: 9.1.2020
+/*******************************************************/
+/*Program Description: This program acts as a linear algebra
+ * calculator. The program itself relies on the implementation
+ * of a 'Line' class to solve for variables, slope, and line
+ * intersection for a line equation in standard format.*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
 using System.Linq;
@@ -12,28 +22,36 @@ namespace Assignment1
         static void Main(string[] args)
         {
             //Declare variables
-            double a;
-            double b;
-            double c;
             double y;
             double x;
+            string val;//This is our resuable variable for input validation function
+            bool parallel;
+            bool perpendicular;
+
 
             //Declare instance of Line object
             Line firstLine;
             firstLine = new Line();
 
+            //Declare second instance of Line object
+            Line secondLine;
+            secondLine = new Line();
+
             Console.WriteLine("If ax + by = c, enter a value for a:");
+
+            /***************************INPUTS FOR FIRST LINE****************************/
             
-            a = double.Parse(Console.ReadLine());//Read user input into local variable: a      
-            firstLine.A = a;//Set value for a
+            val = Console.ReadLine();//Read user input into local variable: a
+            //Set value of object (a) using ValidateNum function
+            firstLine.A = firstLine.ValidateNum(val);
 
             Console.WriteLine("Enter a value for b:");
-            b = double.Parse(Console.ReadLine());
-            firstLine.B = b;
+            val = Console.ReadLine();
+            firstLine.B = firstLine.ValidateNum(val);
 
             Console.WriteLine("Enter a value for c:");
-            c = double.Parse(Console.ReadLine());
-            firstLine.C = c;
+            val = Console.ReadLine();
+            firstLine.C = firstLine.ValidateNum(val);
 
             //Calculate value of x for given y
             Console.WriteLine("Enter a value for y to determine x:");
@@ -47,6 +65,60 @@ namespace Assignment1
 
             //Call function to determine slope of line
             Console.WriteLine($"The slope of the line is: {firstLine.ReturnSlope()}");
+
+
+            /**********************INPUTS FOR SECOND LINE*******************************/
+
+            Console.WriteLine("Enter a value for a of second line: ");
+
+            val = Console.ReadLine();
+            secondLine.A = secondLine.ValidateNum(val);
+
+            Console.WriteLine("Enter a value for b of second line: ");
+
+            val = Console.ReadLine();
+            secondLine.B = secondLine.ValidateNum(val);
+
+            Console.Write("Enter a value for c of second line: ");
+
+            val = Console.ReadLine();
+            secondLine.C = secondLine.ValidateNum(val);
+
+            /**********************OUTPUT MULTI LINE RELATIONSHIPS**********************/
+
+            //Set boolean value for parallel
+            parallel = firstLine.Parallel(firstLine.ReturnSlope(), secondLine.ReturnSlope());
+            //Set boolean value for perpendicular
+            perpendicular = firstLine.Perpendicular(firstLine.ReturnSlope(), secondLine.ReturnSlope());
+
+            //Control for output.
+            if (parallel == true)
+                Console.WriteLine("The two lines are parallel.");
+            else if (perpendicular == true)
+            {
+                Console.WriteLine("The two lines are perpendicular.");
+                //Call Intersection function
+                firstLine.Intersect(
+                    firstLine.A,   //Listed object parameters in cascading order because 
+                    firstLine.B,   //It takes a lot of horizontal line space - readability
+                    firstLine.C,
+                    secondLine.A,
+                    secondLine.B,
+                    secondLine.C);
+            }
+
+            else
+            {
+                Console.WriteLine("The two lines are neither perpendicular nor parallel.");
+                //Call Intersection function
+                firstLine.Intersect(
+                    firstLine.A,   //Listed object parameters in cascading order because 
+                    firstLine.B,   //It takes a lot of horizontal line space - readability
+                    firstLine.C,
+                    secondLine.A,
+                    secondLine.B,
+                    secondLine.C);
+            }
 
         }
     }
