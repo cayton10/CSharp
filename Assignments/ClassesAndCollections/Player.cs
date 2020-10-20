fdeb0cb6546ffcb0f96862aa2abd1e644e8cbddb
Obi-Wan <cayton10@live.marshall.edu>
2020-10-20 16:11:11 -0400

    A4 Complete

--- Assignments/ClassesAndCollections/Player.cs
+++ Assignments/ClassesAndCollections/Player.cs
@@ -35,11 +35,14 @@ namespace ClassesAndCollections
             playerName = name;
         }
 
-        public void PlayGame(string name1, string name2)
+
+        //PlayGame function as described in project outline
+        public void PlayGame(Player name1, Player name2)
         {
             //Check if playing against self
-            if (name1 != name2)
+            if (name1.playerName != name2.playerName)
             {
+
                 //Instantiate player choices of type random
                 Random playerOneChoice = new Random();
                 //Provide second random choice constructor w/ different seed val
@@ -49,13 +52,39 @@ namespace ClassesAndCollections
                 int choice1 = playerOneChoice.Next(1, 4);
                 int choice2 = playerTwoChoice.Next(1, 4);
 
-                Console.WriteLine(name1);
+                //Check for tying condition
+                if(choice1 == choice2)
+                {
+                    //Reroll choice while choices are same
+                    do
+                    {
+                        choice1 = playerOneChoice.Next(1, 4);
+                        choice2 = playerTwoChoice.Next(1, 4);
+                    } while (choice1 == choice2);
+                }
 
-                var weapon1 = (Actions)choice1;
-                Console.WriteLine($"{name1}'s weapon: {weapon1}");
-
-                var weapon2 = (Actions)choice2;
-                Console.WriteLine($"{name2}'s weapon: {weapon2}");
+                //Create logic to find winning condition
+                //Update wins and losses for each player as necessary
+                if(choice1 == 1 && choice2 == 3)
+                {
+                    name1.wins++;
+                    name2.losses++;
+                }
+                else if(choice1 == 2 && choice2 == 1)
+                {
+                    name1.wins++;
+                    name2.losses++;
+                }
+                else if(choice1 == 3 && choice2 == 2)
+                {
+                    name1.wins++;
+                    name2.losses++;
+                }
+                else
+                {
+                    name2.wins++;
+                    name1.losses++;
+                }
 
             }
             else
--- Assignments/ClassesAndCollections/Program.cs
+++ Assignments/ClassesAndCollections/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ClassesAndCollections
 {
@@ -6,12 +8,87 @@ namespace ClassesAndCollections
     {
         static void Main(string[] args)
         {
-            //Create a couple of players to test
+            
+            
 
+            //Create a couple of players to test
             Player player1 = new Player("John");
             Player player2 = new Player("Jerry");
 
-            player1.PlayGame(player1.PlayerName, player2.PlayerName);
+            player1.PlayGame(player1, player2);
+
+
+            string name1 = "Bob";
+            string name2 = "Thomas";
+            string name3 = "Antonio";
+            string name4 = "Brad";
+
+            //Create array to store playerNames
+            string[] playerNames = { name1, name2, name3, name4 };
+                       
+            
+            //Create instance of program so I can call function Players()
+            Program p = new Program();
+
+
+            //Instantiate list of players using prescribed function
+            List<Player> PlayerList = (p.Players(playerNames));
+
+
+
+            //Play games
+            for(int i = 0; i < PlayerList.Count(); ++i)
+            {
+                var firstPlayer = PlayerList[i];
+                
+
+                for(int k = 0; k < 5; k++ )
+                {
+                    int rando;
+
+                    //Do while to generate random players within the list
+                    do
+                    {
+
+                        Random randomIndex = new Random();
+                        rando = randomIndex.Next(0, playerNames.Count());
+
+                    } while (rando == i);
+
+                    //Assign random index to randomPlayer
+                    var randomPlayer = PlayerList[rando];
+                    firstPlayer.PlayGame(firstPlayer, randomPlayer);
+
+                }
+            }
+
+
+            //Use LINQ to display leaderboard
+            var sortedByWins = from player in PlayerList
+                               orderby player.Wins descending
+                               select player;
+            Console.WriteLine("PaperClip, HandGrenade, ThermonuclearDevice Leaderboard:\n");
+
+            foreach (var playerWins in sortedByWins)
+            {
+                Console.WriteLine(playerWins.PlayerName + "'s Wins: " +
+                    playerWins.Wins + ", Losses: " + playerWins.Losses);
+            }
+
+        }
+
+        //Create function to accept any number of player names
+        List<Player> Players(params string[] playerNames)
+        {
+            //Declare out list of players
+            List<Player>allPlayers = new List<Player>();
+
+            foreach (var player in playerNames)
+            {
+                allPlayers.Add(new Player(player));
+            }
+
+            return allPlayers;
         }
     }
 }
