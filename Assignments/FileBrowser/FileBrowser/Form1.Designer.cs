9ae31a96e95aaf29d575a34b153467506f7be6db
obi-wan <cayton10@live.marshall.edu>
2020-11-28 22:09:39 -0500

    start of A6

---
+++ Assignments/FileBrowser/FileBrowser/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace FileBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.browseDirectory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(256, 69);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(246, 298);
+            this.treeView1.TabIndex = 0;
+            // 
+            // browseDirectory
+            // 
+            this.browseDirectory.Location = new System.Drawing.Point(320, 391);
+            this.browseDirectory.Name = "browseDirectory";
+            this.browseDirectory.Size = new System.Drawing.Size(118, 28);
+            this.browseDirectory.TabIndex = 1;
+            this.browseDirectory.Text = "Browse Directories";
+            this.browseDirectory.UseVisualStyleBackColor = true;
+            this.browseDirectory.Click += new System.EventHandler(this.browseDirectory_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.browseDirectory);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button browseDirectory;
+    }
+}
+
